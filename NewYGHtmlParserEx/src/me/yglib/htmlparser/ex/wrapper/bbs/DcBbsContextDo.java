5e8027f6d1ed238df482a7d76e0d2a5f82931ec2
gonni21c <gonni21c@gmail.com>
2014-01-22 12:03:58 +0000

    Initial commit

---
+++ NewYGHtmlParserEx/src/me/yglib/htmlparser/ex/wrapper/bbs/DcBbsContextDo.java
@@ -0,0 +1,15 @@
+package me.yglib.htmlparser.ex.wrapper.bbs;
+
+import java.util.List;
+
+public class DcBbsContextDo {
+	
+	private String context;	// context type is HTML
+	private String title;
+	private String date;
+	private String userName;
+	private int count;
+	
+	private List<DcReplyDo> lstReplies ;
+	
+}
---
+++ NewYGHtmlParserEx/src/me/yglib/htmlparser/ex/wrapper/bbs/DcReplyDo.java
@@ -0,0 +1,10 @@
+package me.yglib.htmlparser.ex.wrapper.bbs;
+
+import java.util.Date;
+
+public class DcReplyDo {
+	private String name;
+	private Date dateTime;
+	private String context;
+	private String extra;
+}
